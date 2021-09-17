b46c554a4d5cfd8fdcb74d608212eeeced8c1618
oq-Yuki-op <o.yukinko0715@gmail.com>
2021-09-18 00:19:27 +0900

    CORS対応

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.models.setting import initialize_db
 from app.routers import arxiv_query_router, paper_stock_router, papers_router
@@ -10,3 +11,16 @@ app.include_router(papers_router)
 app.include_router(paper_stock_router)
 
 initialize_db()
+
+origins = [
+    "https://localhost",
+    "http://localhost"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
